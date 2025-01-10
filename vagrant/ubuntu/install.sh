5614ef49f3eb669f21cf4af756c10a7287de157e
rostikdiv <152973563+rostikdiv@users.noreply.github.com>
2025-01-10 15:06:53 +0200

    Add files via upload

--- vagrant/ubuntu/install.sh
+++ vagrant/ubuntu/install.sh
@@ -1,6 +1,4 @@
 sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get uptade
+sudo apt-get update
 sudo apt install -y openjdk-8-jdk
-
-
-sudo apt-get install -y apache2
+apt-get install -y apache2
